7dc7fbb2a1fd8d1de6c427e52199f306ed485c40
mikam <mika.bobo@ucalgary.ca>
2025-02-12 11:57:44 -0700

    implemented Driver class

--- edu/ucalgary/oop/Driver.java
+++ edu/ucalgary/oop/Driver.java
@@ -1,4 +1,21 @@
 package edu.ucalgary.oop;
 
 public class Driver {
+    private String name;
+
+    public Driver(String name) {
+        setName(name);
+    }
+
+    public Driver(Driver other) {
+        this.name = other.name;
+    }
+
+    public String getname() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
